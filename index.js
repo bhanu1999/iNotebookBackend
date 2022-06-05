a896f4b06f002807c02d35dfafd429024a5a60d4
bhanu1999 <sakinalabhanu99@gmail.com>
2022-06-05 15:01:24 +0530

    creating basic end points

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use('/api/auth',require('./routes/auth.js'))
+app.use('/api/notes',require('./routes/notes.js'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
